4a58783eac7fb4f9e51e6657cabb3570f6e66ee6
Abdinajib <ynaj99@gmail.com>
2024-06-25 16:21:46 -0700

    added proxy code, changed test.html permission

--- proxy_server.py
+++ proxy_server.py
@@ -1,5 +1,27 @@
+from socket import *
+import os
+from datetime import datetime
+
+def handle_req():
+    return
+    ### Logic to serve client if the obj is there else get it from the server
+
+
 def main():
-    print("Hello from proxy")
+    proxy_socket = socket(AF_INET, SOCK_STREAM)
+    server_port = 12345
+    server_ip = 'localhost'
+
+    proxy_socket.bind((server_ip, server_port))
+    proxy_socket.listen(5)
+
+    print("Proxy Server running on port: " + str(server_port))
+
+    while True:
+        clientConn, addr = proxy_socket.accept()
+        handle_req(clientConn)
+
+
 
 if __name__ == "__main__":
     main()
