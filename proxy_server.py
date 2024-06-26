c95509538b0e7bc3b00250bd3936a1795bf9f6b2
Abdinajib <ynaj99@gmail.com>
2024-06-25 18:29:27 -0700

    implemented and optimized proxy

--- proxy_server.py
+++ proxy_server.py
@@ -1,49 +1,66 @@
 from socket import *
-import os
-from datetime import datetime
 import threading
 
 def handle_req(client_socket, server_host, server_port):
     try:
-        #recv the req from client
-        req = client_socket.recv(1024)
+        # Receive the request from the client
+        req = client_socket.recv(1024).decode('utf-8')
+        print(f"Request received from client:\n{req}")
 
-        #create server socket to connect target to server
+        # Modify the request to remove the hostname
+        request_lines = req.split('\r\n')
+        request_line = request_lines[0].split()
+        if len(request_line) > 1:
+            path = request_line[1]
+            if path.startswith('http://'):
+                path = path[path.find('/', 7):]  # Remove http://hostname
+            request_lines[0] = f"{request_line[0]} {path} {request_line[2]}"
+        modified_request = '\r\n'.join(request_lines).encode('utf-8')
+        
+        # Create server socket to connect to the target server
         server_socket = socket(AF_INET, SOCK_STREAM)
         server_socket.connect((server_host, server_port))
 
-        #forward req to the server
-        server_socket.sendall(req)
-    
-    ### Logic to serve client if the obj is there else get it from the server
+        # Forward the modified request to the server
+        server_socket.sendall(modified_request)
 
+        while True:
+            # Receive the response from the server
+            res = server_socket.recv(4096)
+            if len(res) > 0:
+                # Forward the response to the client
+                client_socket.sendall(res)
+            else:
+                break
 
+        # Termination of sockets
+        server_socket.close()
+        client_socket.close()
+    except Exception as e:
+        print(f"Proxy request error: {e}")
+        client_socket.close()
 def main():
     proxy_socket = socket(AF_INET, SOCK_STREAM)
-    proxy_host = 12345
-    proxy_server_host = 'localhost'
-
-
-    server_port = 8080
+    proxy_host = 'localhost'
+    proxy_port = 12345
 
     server_host = 'localhost'
+    server_port = 8080
 
-    proxy_socket.bind((proxy_server_host, proxy_host))
+    proxy_socket.bind((proxy_host, proxy_port))
     proxy_socket.listen(5)
 
-    print("Proxy Server running on port: " + str(proxy_host))
+    print("Proxy Server running on port: " + str(proxy_port))
 
     while True:
         client_socket, addr = proxy_socket.accept()
-        print("conn received from {addr}")
+        print(f"Connection received from {addr}")
 
-        # new thread to handle client request
-        proxy_thread = threading.Thread(target = handle_req, args = (client_socket, server_host, server_port))
+        # New thread to handle client request
+        proxy_thread = threading.Thread(target=handle_req, args=(client_socket, server_host, server_port))
 
-        #starting thread
+        # Start the thread
         proxy_thread.start()
-
-
 
 if __name__ == "__main__":
     main()
