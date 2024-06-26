0b735a3d8598070ab09da5e7ec39757e48f7f576
Abdinajib <ynaj99@gmail.com>
2024-06-25 17:47:10 -0700

    implemented tartiary connection between client-proxy-server

--- proxy_server.py
+++ proxy_server.py
@@ -3,8 +3,18 @@ import os
 from datetime import datetime
 import threading
 
-def handle_req():
-    return
+def handle_req(client_socket, server_host, server_port):
+    try:
+        #recv the req from client
+        req = client_socket.recv(1024)
+
+        #create server socket to connect target to server
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        server_socket.connect((server_host, server_port))
+
+        #forward req to the server
+        server_socket.sendall(req)
+    
     ### Logic to serve client if the obj is there else get it from the server
 
 
